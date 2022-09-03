d1e4353f3f9c38c8c37ecd293a2ea8470ee236ac
mozop <you@example.com>
2022-09-04 02:37:15 +0600

    style added

--- app.js
+++ app.js
@@ -1,23 +1,25 @@
 // Get Data
 fetch('https://restcountries.com/v3.1/all')
-.then(response => response.json())
+    .then(response => response.json())
     .then(data => displayCountries(data))
 
 //Display Data
 const displayCountries = async countries => {
     const countriesDiv = document.getElementById('countries');
+
     for (let i = 0; i < countries.length; i++) {
         const country = countries[i];
         const countryDiv = document.createElement('div');
-        const name = document.createElement('h3');
-        name.innerText = country.name.common;
-        const capital = document.createElement('p');
-        capital.innerText = country.capital
-  
+        countryDiv.className = 'country';
+
+        const countryInfo = `
+            <h3 class = "country-name">${country.name.common}</h3>
+            <p class = "country-capital">${country.capital}</p>
+        `
+
+        countryDiv.innerHTML = countryInfo;
+
         countriesDiv.appendChild(countryDiv);
-        countryDiv.appendChild(name);
-        countryDiv.appendChild(capital);
-        console.log(country.name);
     }
 
 };
